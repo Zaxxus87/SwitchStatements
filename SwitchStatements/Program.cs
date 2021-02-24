14d480aed9a987ce5c8649882c133da944e0d94a
Jeff Baker <jeffrey1871@gmail.com>
2021-02-23 20:05:08 -0600

    Next version

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string subject;
+            Console.WriteLine("What is your favorite " +
+                "subject(Math/Science/History/English/Programming)");
+            subject = Console.ReadLine();
+
+            switch(subject)
+            {
+                case "Math":
+                    Console.WriteLine("Knowing your Numbers");
+                    break;
+                case "Science":
+                    Console.WriteLine("Matter of taste");
+                    break;
+                case "History":
+                    Console.WriteLine("We don't want to repeat ourselves");
+                    break;
+                case "English":
+                    Console.WriteLine("Is it two, to, or too");
+                    break;
+                default:
+                    Console.WriteLine("Who doesn't like computers?");
+                    break;
+            }
+        }
+    }
+}
